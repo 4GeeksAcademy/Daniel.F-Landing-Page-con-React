28091d1dfe649d83c42d6d5e1ddf5926f09c58f2
Daniel Fabelo <daniel-fabelo-santana@outlook.es>
2026-04-15 20:03:59 +0000

    First Commit

---
+++ src/js/components/jumbotron.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Jumbotron = () => {
+	return (
+		<div className="jumbotron col-md-6 offset-md-3">
+			<h1 className="display-4 alaing item-centre">¡Hola, mundo!</h1>
+			<p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ab doloribus, nostrum dignissimos ducimus earum reiciendis exercitationem sed accusantium sapiente excepturi atque dicta temporibus obcaecati praesentium esse quas quam delectus!</p>
+				<p>Puedes personalizarlo a tu gusto.</p>
+				<a className="btn btn-primary btn-lg" href="#" role="button">Aprender más</a>
+		</div>
+
+	);
+};
+
+export default Jumbotron;
