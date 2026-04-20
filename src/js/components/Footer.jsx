53483bd1451ea37d0f10b5136b393e1956cfeea9
Daniel Fabelo <daniel-fabelo-santana@outlook.es>
2026-04-20 17:55:15 +0000

    again

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-
 const Footer = () => {
-    return (
-        <footer className="bg-dark text-white text-center py-4">
-            <div className="container">
-                <p> <small>Copyright</small></p>
-            </div>
-        </footer>
-    );
+	return (
+		<footer className="bg-dark text-white text-center py-4 mt-4">
+			<p className="mb-0">
+				Copyright © Your Website 2026
+			</p>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,25 +1,59 @@
-import React from "react";
-import Navbar from './navbar'
-import Jumbotron from './jumbotron'
-import Card from './card'
-import Footer from './Footer'
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Card from "./Card";
+import rigo from "../../img/rigo-baby.jpg";
+import Footer from "./Footer";
 
-//create your first component
+
 const Home = () => {
 	return (
-		<div className = "text-center">
+		<>
 			<Navbar />
-			<Jumbotron />
-			<div className="card-container d-flex m-auto">
-			<Card/>
-			<Card/>
-			<Card/>
-			<Card/>
+
+			<div className="container mt-4">
+				<Jumbotron />
+
+				<div className="row mt-4">
+					<div className="col-12 col-md-6 col-lg-3 mb-4">
+						<Card
+							image={rigo}
+							title="Primera card"
+							text="Texto de la primera card."
+							buttonText="Ver más"
+						/>
+					</div>
+
+					<div className="col-12 col-md-6 col-lg-3 mb-4">
+						<Card
+							image={rigo}
+							title="Segunda card"
+							text="Texto de la segunda card."
+							buttonText="Explorar"
+						/>
+					</div>
+
+					<div className="col-12 col-md-6 col-lg-3 mb-4">
+						<Card
+							image={rigo}
+							title="Tercera card"
+							text="Texto de la tercera card."
+							buttonText="Entrar"
+						/>
+					</div>
+
+					<div className="col-12 col-md-6 col-lg-3 mb-4">
+						<Card
+							image={rigo}
+							title="Cuarta card"
+							text="Texto de la cuarta card."
+							buttonText="Más info"
+						/>
+					</div>
+				</div>
 			</div>
-			<div className = "position-static bottom start-50">
-			<Footer/>
-			</div>
-		</div>
+
+			<Footer />
+		</>
 	);
 };
 
